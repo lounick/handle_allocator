24d037364d3d103b80c5dd994e332b749182ae0a
Niko <lou.nick@gmail.com>
2021-02-12 18:41:02 +0100

    Add equality comparison for handles

--- include/handle_allocator/handle.h
+++ include/handle_allocator/handle.h
@@ -6,6 +6,9 @@ namespace handle_allocator {
 struct Handle {
   std::size_t index;
   std::size_t pattern;
+  inline bool operator==(const Handle& rhs) {
+    return this->index == rhs.index && this->pattern == rhs.pattern;
+  }
 };
 
 }  // namespace handle_allocator
---
+++ test/gtest_handle.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+#include <handle_allocator/handle.h>
+
+TEST(TEST_HANDLE, TEST_HANDLE_EQUALS_EQUAL_HANDLES) {
+  handle_allocator::Handle h1{0, 0};
+  handle_allocator::Handle h2{0, 0};
+  EXPECT_TRUE(h1 == h2);
+}
+
+TEST(TEST_HANDLE, TEST_HANDLE_EQUALS_INEQUAL_HANDLES_INDEX) {
+  handle_allocator::Handle h1{0, 0};
+  handle_allocator::Handle h2{1, 0};
+  EXPECT_FALSE(h1 == h2);
+}
+
+TEST(TEST_HANDLE, TEST_HANDLE_EQUALS_INEQUAL_HANDLES_PATTERN) {
+  handle_allocator::Handle h1{0, 0};
+  handle_allocator::Handle h2{0, 1};
+  EXPECT_FALSE(h1 == h2);
+}
+
+TEST(TEST_HANDLE, TEST_HANDLE_EQUALS_INEQUAL_HANDLES_BOTH) {
+  handle_allocator::Handle h1{0, 0};
+  handle_allocator::Handle h2{1, 1};
+  EXPECT_FALSE(h1 == h2);
+}
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
